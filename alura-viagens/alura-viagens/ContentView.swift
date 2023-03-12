d900d1f404504be95b71e8b886b27cca87f3e667
Allan Brandão <allanbrandao@MacBook-Air-de-Allan.local>
2023-03-11 22:23:13 -0300

    Added the ContentView

--- alura-viagens/alura-viagens/ContentView.swift
+++ alura-viagens/alura-viagens/ContentView.swift
@@ -9,16 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     @Binding var document: alura_viagensDocument
-
+    
     var body: some View {
         VStack {
-                Text("Teste")
-        }
+            Spacer()
+            HStack {
+                Spacer()
+                Text("Crias Viagens")
+                    .foregroundColor(Color.white)
+                Spacer()
+            }
+            Spacer()
+            List {
+                
+            }
+        }.background(Color.black)
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView(document: .constant(alura_viagensDocument()))
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView(document: .constant(alura_viagensDocument()))
+        }
     }
 }
