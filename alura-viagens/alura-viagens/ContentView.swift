14b1164c732097d64638fb115c0779368b656634
Allan Brandão <allanbrandao@MacBook-Air-de-Allan.local>
2023-03-14 17:38:12 -0300

    Added the colors and buttons on screen

--- alura-viagens/alura-viagens/ContentView.swift
+++ alura-viagens/alura-viagens/ContentView.swift
@@ -11,19 +11,55 @@ struct ContentView: View {
     @Binding var document: alura_viagensDocument
     
     var body: some View {
-        VStack {
-            Spacer()
-            HStack {
-                Spacer()
-                Text("Crias Viagens")
-                    .foregroundColor(Color.white)
-                Spacer()
-            }
-            Spacer()
-            List {
+        
+        GeometryReader { view in
+            
+            // MARK: - VStack Principal
+            
+            VStack {
                 
+                // MARK: - VStack Header
+                
+                VStack {
+                    Text("AB Viagens")
+                        .font(.custom("Avenir Black", size: 25))
+                    Text("As melhores viagens")
+                        .font(.custom("Avenir Black", size: 15))
+                }
+                .frame(width: view.size.width,
+                       height: 70,
+                       alignment: .top
+                )
+                .background(Color.init("Blue700"))
+                
+                HStack {
+                    Button(action: {}) {
+                        Text("Pacotes")
+                            .font(.custom("Avenir Medium", size: 17))
+                            .foregroundColor(.black)
+                    }
+                    .frame(width: 80, height: 25)
+                    .background(Color.init("LBlue600"))
+                    .cornerRadius(5)
+                    
+                    Button(action: {}) {
+                        Text("Hotéis")
+                            .font(.custom("Avenir Medium", size: 17))
+                            .foregroundColor(.black)
+                    }
+                    .frame(width: 80, height: 25)
+                    .background(Color.init("LBlue600"))
+                    .cornerRadius(5)
+                }
+                
+                List {
+                    Text("Rio de Janeiro")
+                    Text("Ceará")
+                    Text("Sergipe")
+                    Text("Santa Catarina")
+                }
             }
-        }.background(Color.black)
+        }
     }
     
     struct ContentView_Previews: PreviewProvider {
